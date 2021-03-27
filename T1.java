f29c2c9f5851804a143ccd5ce229f550fa18b563
Daniela-mar <81478966+Daniela-mar@users.noreply.github.com>
2021-03-27 18:57:22 +0200

    Add files via upload

---
+++ T1.java
@@ -0,0 +1,27 @@
+
+public class numbers {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nNumber divisable by 3!");		
+	    for (int i=1; i<100; i++) {
+		if (i%3==0) 
+		System.out.print(i +"\n");			
+	}			
+			
+	System.out.println("\nNumber divisable by 5!");
+	for (int i=1; i<100; i++) {
+		if (i%5==0) System.out.print(i +"\n");			
+		
+	}
+	System.out.println("\n");
+}
+
+
+}
+
+		
+
+
+
+
